d1525de8c41016c19c34b875db2ecbbaaf25f7c4
LargeCow <will@zgoda.com>
2020-11-30 22:51:57 -0700

    added q2

--- 1.py
+++ 1.py
@@ -1,4 +1,17 @@
 
+# question 1
+with open('inputs/input_1.txt', 'r') as input_file:
+    lines = input_file.readlines()
+    for i in lines:
+        for x in lines:
+                x, i = int(x), int(i)
+                res = x +i
+                if res == 2020:
+                    print(x * i)
+
+input_file.close()
+
+# question 2
 with open('inputs/input_1.txt', 'r') as input_file:
     lines = input_file.readlines()
     for i in lines:
@@ -8,3 +21,5 @@ with open('inputs/input_1.txt', 'r') as input_file:
                 res = x + i + y
                 if res == 2020:
                     print(x * i * y)
+                    break
+input_file.close()
